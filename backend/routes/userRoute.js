e91a86182f08d59dec3a15fc6b816f6c657f6d93
nobelium24 <ogunbaja07@gmail.com>
2023-05-04 13:43:59 +0100

    encrypting passwords

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,9 @@
 const express = require("express")
-const { land, signUp } = require("../controllers/userController")
+const { land, signUp, resetPassword, signIn } = require("../controllers/userController")
 const userRouter = express.Router()
 
 userRouter.get("/", land)
 userRouter.post("/signup", signUp)
+userRouter.post("/resetpassword",resetPassword)
+userRouter.post('/signin', signIn)
 module.exports = userRouter
